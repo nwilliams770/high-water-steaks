d8c5168f1526c65e6f00308ac33dad3d68447fd2
Nicholas Williams <nwilliams770@gmail.com>
2019-02-07 14:38:39 -0800

    refactor data shape, bars rendering

--- src/components/bars.jsx
+++ src/components/bars.jsx
@@ -8,12 +8,72 @@ class Bars extends React.Component {
     }
 
     createGroups() {
-        
+        const result = {"poultry": [], "pork": [], "beef": [], "mutton": []};
+        const { data, scales, margins, svgDimensions, emojis } = this.props;
+        const { xScale, yScale } = scales; 
+
+        for (let i = 0; i < data.length; i++) {
+            for (let subject in result) {
+                let subjectStack = data[i].stacks.filter(obj => obj.subject === subject)[0];
+                result[subject].push(
+                    <rect
+                        key={`${subject}-${i}`}
+                        x={xScale(data[i].emoji)}
+                        y={yScale(subjectStack.y1)}
+                        // height={svgDimensions.height - margins.bottom - yScale(subjectStack.y0)}
+                        height={ yScale(subjectStack.y0) - yScale(subjectStack.y1) }
+                        width={xScale.bandwidth()}
+                    />
+                )
+            }
+
+        }
+
+        // for (let subject in data) {
+        //     result[subject] = Object.keys(data[subject]).map( (country, i) => {
+        //         // iterating over country names
+        //         return (
+        //             <rect
+        //                 key={`${subject}-${i}`}
+        //                 x={xScale(emojis[country])}
+        //                 y={yScale(data[subject][country])}
+        //                 height={svgDimensions.height - margins.bottom - yScale(data[subject][country])}
+        //                 width={xScale.bandwidth()}
+        //                 fill={subject === "beef" ? "pink" : "yellow"}
+        //             />
+        //         )
+        //     })
+        // }
+
+        // console.log("result");
+        return result
+
     }
 
     render() {
+        const barGroups = this.createGroups();
+
+        return (
+            <g className="bars">
+                <g className="poultry">
+                    {barGroups['poultry']}
+                </g>
+                <g className="pork">
+                    {barGroups['pork']}
+                </g>
+                <g className="beef">
+                    {barGroups['beef']}
+                </g>
+                <g className="mutton">
+                    {barGroups['mutton']}
+                </g>
+            </g>
+        )
 
     }
 
 
+
 }
+
+export default Bars;
--- src/components/content.jsx
+++ src/components/content.jsx
@@ -3,6 +3,7 @@ import consumption from '../data/meat_consumption.csv';
 import { csv } from 'd3-fetch';
 import { countries } from 'country-data';
 import BarChart from './bar_chart';
+import SelectorModule from './selector_module';
 
 // TO DO: 
 // Add methods to sort data
@@ -14,14 +15,15 @@ class Content extends React.Component {
         this.state = {
             year: '2019' // default value for this year
         };
+        this.updateYear = this.updateYear.bind(this);
     }
 
     componentDidMount() {
-        // data shape:
-            // year => subject => country => value
         this.getData().then((parsedData) => {
             this.setState({ 
-                data: parsedData.data,
+                // uses emoji slice for adding to objects
+                data: this.processData(parsedData),
+                // saving this slice for easy passing to axes
                 emojis: parsedData.emojis
             });
         })
@@ -31,7 +33,7 @@ class Content extends React.Component {
     async getData() {
         const data = await csv(consumption);
         const yearRange = [1995, 2025];
-        const result = this.instantiateStarterObject(yearRange);
+        const unsortedData = this.instantiateStarterObject(yearRange);
         
         for (let i = 0; i < data.length; i++) {
             // Filter out:
@@ -40,15 +42,59 @@ class Content extends React.Component {
             //  - Different measurement tips// currently KG / CAPITA
             if (typeof countries[data[i].location] !== 'undefined' && yearRange[0] <= parseInt(data[i].year) && data[i].measure === "KG_CAP") {
                 let countryName = countries[data[i].location].name;
-                result['data'][data[i].year][data[i].subject.toLowerCase()][countryName] = data[i].value
+                unsortedData['data'][data[i].year][data[i].subject.toLowerCase()][countryName] = data[i].value
                 // maybe refactor, kind of annoying
-                result['emojis'][[countries[data[i].location].name]] = countries[data[i].location].emoji;
+                unsortedData['emojis'][[countries[data[i].location].name]] = countries[data[i].location].emoji;
 
             }
         }
-        console.log("Data")
-        console.log(result);
-        return result
+
+
+        return unsortedData
+    }
+
+
+    processData(unsortedData) {
+        const sorted = this.sortDataByCountry(unsortedData);
+        const stacked = this.calcStacks(sorted);
+        return stacked;
+    }
+
+    calcStacks(sortedData) {
+        const orderedSubjects = ["poultry", "pork", "mutton", "beef"]
+        for (let year in sortedData) {
+            sortedData[year].forEach(dataSet => {
+                let y0 = 0;
+                dataSet.stacks = orderedSubjects.map((subject) => {
+                    return ({ subject: subject, y0: y0, y1: y0 += +dataSet[subject] })
+                })
+                dataSet.total = dataSet.stacks[dataSet.stacks.length - 1].y1
+            })
+        }
+        return sortedData;
+    }
+
+    sortDataByCountry(unsortedData) {
+        const { data, emojis } = unsortedData;
+        const result = {}
+
+        for (let year in data) {
+            let temp = {};
+            for (let subject in data[year]) {
+                for (let country in data[year][subject]) {
+                    if (temp[country]) {
+                        temp[country][subject] = data[year][subject][country];
+                    }
+                    else {
+                        temp[country] = {name: country, emoji: emojis[country], [subject]: data[year][subject][country]};
+                    }
+                }
+
+
+            }
+            result[year] = Object.values(temp);
+        }
+        return result;
     }
 
     instantiateStarterObject(yearRangeArray) {
@@ -77,6 +123,10 @@ class Content extends React.Component {
         const { data, year, emojis } = this.state;
         return (
             <div className='content'>
+                <SelectorModule
+                    years={Object.keys(data)}
+                    updateYear={this.updateYear}
+                />
                 <BarChart
                     svgDimensions={svgDimensions}
                     margins={margins}
