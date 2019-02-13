25276d41f6312ac5795102687b82a2b507bacd44
Nicholas Williams <nwilliams770@gmail.com>
2019-02-13 13:03:34 -0800

    add helper func to format republics

--- src/components/tooltip.jsx
+++ src/components/tooltip.jsx
@@ -3,15 +3,19 @@ import React from 'react';
 export function showAxisTooltip(evt, key) {
     const tooltip = document.querySelector('.tooltip');
 
-    tooltip.innerHTML = key[evt.target.innerHTML];
-    tooltip.style.display = 'block';
-    tooltip.style.left = evt.pageX + 10 + 'px';
-    tooltip.style.top = evt.pageY - 25 + 'px';
+    tooltip.innerHTML = formatCountry(key[evt.target.innerHTML]);
+    tooltip.style.display = 'inline-block';
+    tooltip.style.left = evt.pageX + 'px';
+    tooltip.style.top = evt.pageY + 15 + 'px';
 }
   
 export function hideTooltip() {
     const tooltip = document.querySelector('.tooltip');
     tooltip.style.display = 'none';
+}
+
+function formatCountry(country) {
+    return country.includes(",") ? country.split(",").reverse().join().replace(",", " ") : country
 }
 
 const Tooltip = () => (
