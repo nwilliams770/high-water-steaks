87c49a3ca94524aff0acf4770a867b06c914127c
Nicholas Williams <nwilliams770@gmail.com>
2019-02-20 16:23:54 -0800

    Updates

---
+++ precache-manifest.bbd5e587bae2026aa0fc1d710d3dce93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "003338b1a591494d6b6f83d7c2a41d61",
+    "url": "/high-water-steaks/static/media/meat_water_cost.003338b1.csv"
+  },
+  {
+    "revision": "4e3b16bacc70859df0fe9c5d176442eb",
+    "url": "/high-water-steaks/static/media/meat_consumption.4e3b16ba.csv"
+  },
+  {
+    "revision": "73fa64902dc3c3fe3e15",
+    "url": "/high-water-steaks/static/js/runtime~main.73fa6490.js"
+  },
+  {
+    "revision": "ec3136ff5605d668c606",
+    "url": "/high-water-steaks/static/js/main.ec3136ff.chunk.js"
+  },
+  {
+    "revision": "4daa644b32a75905c8d2",
+    "url": "/high-water-steaks/static/js/1.4daa644b.chunk.js"
+  },
+  {
+    "revision": "ec3136ff5605d668c606",
+    "url": "/high-water-steaks/static/css/main.857b93c5.chunk.css"
+  },
+  {
+    "revision": "2fd43a66e9d31d87ecd166d90eee4e7f",
+    "url": "/high-water-steaks/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/high-water-steaks/precache-manifest.bbd5e587bae2026aa0fc1d710d3dce93.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/high-water-steaks/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
